0826da812bb0888544049f8304c364e141462523
João Victor <92130316+sousadevelop@users.noreply.github.com>
2025-11-06 11:36:15 +0000

    adicionado e configurado o  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Faltam 5 dias para nossos 3 anos de casamento...💑</h1>;
+function Home() {
+  return <h1>Faltam 5 dias para nossos 3 anos de casamento...💑</h1>;
 }
 
 export default Home;
