ce3a5d0c7e08c22c25b03b39533c06cc4796d5f4
João Victor <92130316+sousadevelop@users.noreply.github.com>
2025-11-09 23:38:05 +0000

    subindo a API

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function status(req, res) {
+  res.status(200).json({ chave: "coração quero vc" });
+}
+
+export default status;
---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,4 @@
+test("GET /api/v1/status should return status 200", async () => {
+  const resp = await fetch("http://localhost:3000/api/v1/status");
+  expect(resp.status).toBe(200);
+});
